290d53bfd454c46159f0a00d124de8f02a50c06f
XxAsentrixX <asentrixtn@gmail.com>
2023-09-24 16:20:29 +0100

    a commit

--- app_server/controllers/main.js
+++
@@ -1,8 +0,0 @@
-/* GET HOMEPAGE */
-const index = (req, res) => { // creates index function
-    res.render('index',{title: 'Express'}) ; // includes controller code for the homepage
-} ; 
-// exposes the index function as amethod
-module.exports = {
-    index
-} ; 
